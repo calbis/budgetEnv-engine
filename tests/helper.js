5a18ff3cbd834894a020a1c3aa2c369888f70a9c
Calvin Bishop <calbis@gmail.com>
2017-01-28 09:28:41 -0500

    now requiring https by redirecting all http traffic to https.

--- tests/helper.js
+++ tests/helper.js
@@ -1,3 +1,31 @@
 'use strict';
 
-exports.baseUrl = "http://localhost:3000/";
+
+exports.baseUrl = function () {
+	var port = exports.ensureValidPort( process.env.HTTPS_PORT ) || 8443;
+	return 'https://localhost:' + port + '/';
+};
+
+exports.baseUrlHttp = function () {
+	var port = exports.ensureValidPort( process.env.HTTP_PORT ) || 8080;
+	return 'http://localhost:' + port + '/';
+};
+
+exports.needleOptions = {
+	compressed: true,
+	rejectUnauthorized: false
+};
+
+exports.ensureValidPort = function ( port ) {
+	var p = parseInt( port, 10 );
+
+	if ( isNaN( p ) ) {
+		return false;
+	}
+
+	if ( p >= 0 ) {
+		return p;
+	}
+
+	return false;
+};
--- tests/index.js
+++ tests/index.js
@@ -1,17 +1,15 @@
-/* global describe, it */
-
 'use strict';
 
-var helper = require( './helper.js' );
+var helper = require( './helper' );
 var expect = require( "chai" ).expect;
 var needle = require( "needle" );
 
 
-describe( "BudgetEnv Engine", function() {
-	describe( "Should get a response back from the server", function() {
-		it( "servers response", function( done ) {
-			needle.get( helper.baseUrl,
-				function( err, res ) {
+describe( "BudgetEnv Engine", function () {
+	describe( "Should get a response back from the server", function () {
+		it( "servers response", function ( done ) {
+			needle.get( helper.baseUrl(), helper.needleOptions,
+				function ( err, res ) {
 					expect( err ).to.not.exist;
 					expect( res ).to.exist;
 
@@ -20,13 +18,26 @@ describe( "BudgetEnv Engine", function() {
 		} );
 	} );
 
-	describe( "Should get a 404 error when a valid route/file is not found", function() {
-		it( "returns status 404", function( done ) {
-			needle.get( helper.baseUrl + 'asdfg.hjk',
-				function( err, res ) {
+	describe( "Should get a 404 error when a valid route/file is not found", function () {
+		it( "returns status 404", function ( done ) {
+			needle.get( helper.baseUrl() + 'asdfg.hjk', helper.needleOptions,
+				function ( err, res ) {
 					expect( err ).to.not.exist;
 					expect( res ).to.exist;
 					expect( res.statusCode ).to.equal( 404 );
+
+					done();
+				} );
+		} );
+	} );
+
+	describe( "Should be redirected from http to https", function () {
+		it( "Returns status 301", function ( done ) {
+			needle.get( helper.baseUrlHttp(), helper.needleOptions,
+				function ( err, res ) {
+					expect( err ).to.not.exist;
+					expect( res ).to.exist;
+					expect( res.statusCode ).to.equal( 301 );
 
 					done();
 				} );
